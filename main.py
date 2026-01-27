fe68cb8c4322ebe9977fda4bfcf56fe3790e005f
michaelwsd <a0416517630@icloud.com>
2026-01-27 11:53:14 +1100

    added main

---
+++ main.py
@@ -0,0 +1,11 @@
+from scraper import scrape_all_companies
+
+def run():
+    # 1. get the list of companies from salesforce
+    # 2. run scrape function to scrape all companies
+    # 3. push result json to salesforce dashboard
+    # 4. clean up docs
+    pass
+
+if __name__ == "__main__":
+    run()
